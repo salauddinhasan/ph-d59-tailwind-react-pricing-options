16d9174b080998450a72d7f8b213b86137cec272
salauddin <salauddin@hasan.com>
2026-03-19 13:11:15 +0600

    ✨ Initial commit: Tailwind React pricing project

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+  import React from 'react';
+   
+
+  
+  const App = () => {
+    return (
+      <div>
+        <h1 className='text-center p-4 font-bold text-3xl'>hello dev</h1>
+        <p className="text-xl font-bold text-center">My Name is salauddin kdan</p>
+      </div>
+    );
+  };
+  
+  export default App;
