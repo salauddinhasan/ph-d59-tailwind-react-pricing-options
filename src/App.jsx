7d2ec4efe01369dd7c32f61c8f725c4e33724ff5
salauddin <salauddin@hasan.com>
2026-03-19 15:27:46 +0600

    Create NavBar component with dynamic links

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,15 @@
   import React from 'react';
+import DaisyNav from './component/DaisyNav/DaisyNav';
+import NavBar from './component/DaisyNav/NavBar/NavBar';
    
 
   
   const App = () => {
     return (
-      <div>
-        <h1 className='text-center p-4 font-bold text-3xl'>hello dev</h1>
-        <p className="text-xl font-bold text-center">My Name is salauddin kdan</p>
-      </div>
+      <header>
+        <NavBar/>
+          <DaisyNav/>
+      </header>
     );
   };
   
